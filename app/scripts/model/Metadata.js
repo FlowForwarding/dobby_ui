617215334c733d991b23ec21525ed010f486588e
Aliaksandr Kazarez <kozorezal@gmail.com>
2015-06-16 16:57:40 +0300

    Recursive metadata display for objects

--- app/scripts/model/Metadata.js
+++ app/scripts/model/Metadata.js
@@ -1,23 +1,26 @@
 
 class Metadata {
     constructor(metadata) {
-        this.map = new Map();
-        for (let key of Object.keys(metadata)) {
-            this.map.set(key, metadata[key]);
-        }
-    }
-
-    get(key) {
-        return this.map.get(key);
+        this.map = metadata;
     }
 
     display() {
         var result = [];
 
-        for (let [key, value] of this.map) {
-            result.push(`${key}: ${value.value}`);
+        function displayObject(obj) {
+            var res = ['<ul>'];
+            for (let [key, value] of Object.entries(obj)) {
+                var val = value;
+                if (typeof val === 'object') {
+                    val = displayObject(val);
+                }
+                res.push(`<li>${key}: ${val}</li>`);
+            }
+            res.push('</ul>');
+            return res.join("");
         }
 
+        result.push(displayObject(this.map));
 
         return result.join("</br>");
     }
